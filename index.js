32fc679bb7f1a35f739c3fe0cbe41f8053c1bf2b
Leeza <leezaverse@gmail.com>
2026-06-03 19:58:18 +0530

    Minor: Change tab size to 4

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 
 app.get('/hello', (req, res) => {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
- app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
