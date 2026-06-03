44377387add031f48fd0a26828ce2ebee72c6ed5
Leeza <leezaverse@gmail.com>
2026-06-03 19:54:10 +0530

    Initialize express

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const PORT = 3000;
+
+const app = express();
+app.use(cors());
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+});
+
+ app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
